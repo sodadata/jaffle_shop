ef3653aab01b76ee2b58747071aeeffd07fb6885
Bastien Boutonnet <bastien.b1@gmail.com>
2022-01-09 14:37:59 +0100

    add custom schema override (#3)
    * remove duplicate order
    
    * add custom schema
    
    * add a string to cause a new build

--- models/orders.sql
+++ models/orders.sql
@@ -43,7 +43,8 @@ final as (
 
         {% endfor -%}
 
-        order_payments.total_amount as amount
+        order_payments.total_amount as amount,
+        'whohoo final' as final_words
 
     from orders
 
